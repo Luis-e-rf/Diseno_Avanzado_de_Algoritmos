ca76bad45d938d5b416d25f080a455a9bb66c6a7
Yurakc <luis8866@hotmail.com>
2021-10-28 22:02:27 -0500

    algoritmo_fuerza_bruta 2/3

---
+++ algoritmo_fuerza_bruta.py
@@ -0,0 +1,39 @@
+import math
+import time
+import random
+# escribir una funcion para calcular la distancia entre dos puntos
+
+
+def calcular_distancia(A, B):
+    distancia = math.sqrt((B[0]-A[0])**2 + (A[1]-B[1])**2)
+    return distancia
+
+
+print(calcular_distancia((1, 0), (0, 0)))
+
+# escribir una funcion que compare todos los puntos entre si
+# puntos = [(2, 3), (7, 2), (1, 0), (-4, 8), (-3, -9), (0, 0), (-1, -5)]
+cordX = [random.randint(-500, 500) for i in range(3000)]
+cordY = [random.randint(-500, 500) for i in range(3000)]
+puntos = []
+for i in range(len(cordX)):
+    puntos.append((cordX[i], cordY[i]))
+
+
+def puntos_cercanos(nube_puntos):
+    distancia_ref = math.inf
+    for i in range(len(nube_puntos)):
+        for j in range(i+1, len(nube_puntos)):
+            distancia_menor = calcular_distancia(
+                nube_puntos[i], nube_puntos[j])
+            if distancia_menor < distancia_ref:
+                distancia_ref = distancia_menor
+                par_cercano = (nube_puntos[i], nube_puntos[j])
+    return distancia_ref, par_cercano
+
+
+inicio = time.time()
+par = puntos_cercanos(puntos)
+fin = time.time()
+print(fin - inicio)
+print(par)
